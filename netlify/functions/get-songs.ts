8d6b943a555babefba5b51490595883bb93f4607
Harsh Pandey <harshp9026@gmail.com>
2026-01-29 12:55:23 +0530

    fix(functions): use correct env variables for cloudinary config

--- netlify/functions/get-songs.ts
+++ netlify/functions/get-songs.ts
@@ -3,9 +3,9 @@ import { Handler } from '@netlify/functions';
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.REACT_APP_CLOUDINARY_API_KEY,
-  api_secret: process.env.REACT_APP_CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const handler: Handler = async (event, context) => {
